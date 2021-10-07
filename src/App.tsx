13334470ba6253e70c431133c3c3bc0c8a5efb6f
Gary Leija <gleija@seek.com.au>
2021-10-07 15:16:32 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NetflixNavigation from './components/NetflixNavigation';
+
+const App: React.FunctionComponent = () => {
+  return <NetflixNavigation />;
+};
+
+export default App;
---
+++ src/components/NetflixNavigation.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './NetflixNavigation.module.css';
+
+const NetflixNavigation: React.FunctionComponent = () => <></>;
+
+export default NetflixNavigation;
